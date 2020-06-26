28751753b9e0309fec18347ba9ed0ab311474287
Allison Sadin <aesadin@gmail.com>
2020-06-26 11:37:03 -0700

    pass fist test for class constructor

---
+++ src/agecalculator.js
@@ -0,0 +1,8 @@
+export class AgeCalculator {
+  constructor (userAge, userLifeExpectancy, planetYearsInEarthYears) {
+    this.userAge = userAge;
+    this.userLifeExpectancy = userLifeExpectancy;
+    this.planetYearsInEarthYears = planetYearsInEarthYears;
+
+  }
+};
