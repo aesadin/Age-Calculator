f7ac3f7516239e7475a3598e43e3cc94a4e8cc28
aesadin <65793229+aesadin@users.noreply.github.com>
2020-07-05 17:09:01 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import './styles.css';
 $(document).ready(function() {
   $(".formOne").submit(function(event) {
     event.preventDefault();
-    const inputPlanet = $("input:radio[name=planet]:checked").val();
-    const inputtedAge = $("input#age").val();
-    const inputtedLifeExpectancy = $("input#life").val();
+   
   });
 });
